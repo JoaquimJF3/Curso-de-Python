a85dfc94717da3fd870235b5fc2ec44b02cc65f0
JoaquimJF3 <joaquimfernando683@gmail.com>
2021-12-08 23:26:00 +0000

    Alterando
    Change
    
    Co-Authored-By: Fernandes Joaquim <92488147+Joaquim20@users.noreply.github.com>

--- desafios de python/d001.py
+++
@@ -1,5 +0,0 @@
-print('-=-'*5, '\033[1;32;40mDESAFIO 001\033[m', '-=-'*5)
-n1 = int(input('Digite o primeiro valor: '))
-n2 = int(input('Digite o segundo valor: '))
-soma = n1 + n2
-print('A soma entre {} e {} é igual a {}!'.format(n1, n2, soma))
